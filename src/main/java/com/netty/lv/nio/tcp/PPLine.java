492b7f61b2a0d228577944f32ca4838c1c67aba5
lvpf <1359706026@qq.com>
2021-09-08 11:42:32 +0800

    eee

--- src/main/java/com/netty/lv/nio/tcp/PPLine.java
+++ src/main/java/com/netty/lv/nio/tcp/PPLine.java
@@ -1,5 +1,7 @@
 package com.netty.lv.nio.tcp;
 
+import lombok.extern.slf4j.Slf4j;
+
 public class PPLine {
 
 	private MyChannel myChannel;
@@ -14,17 +16,31 @@ public class PPLine {
 		PPHandler handler = new PPHandler();
 		this.headCtx = new HandlerContext(handler,myChannel);
 		this.tailCtx = new HandlerContext(handler,myChannel);
+//		构建链表
+		this.headCtx.next = this.tailCtx;
+		this.tailCtx.prev = this.headCtx;
 	}
+
+	public void addLast(Handler handler){
+		HandlerContext context = new HandlerContext(handler,myChannel);
+		HandlerContext p = this.tailCtx.prev;
+		p.next = context;
+		context.prev = p;
+		context.next = tailCtx;
+		tailCtx.prev = context;
+	}
+
+	@Slf4j
 	public class PPHandler implements Handler{
 
 		@Override
 		public void channelRead(HandlerContext ctx, Object msg) {
-			System.out.println("PPHandler:channelRead");
+			log.debug(msg.toString());
 		}
 
 		@Override
 		public void write(HandlerContext ctx, Object msg) {
-
+			log.debug(msg.toString());
 		}
 
 		@Override
